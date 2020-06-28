37f2fc31fe718c2fbc18701a579533bb2ec01a09
gauthamns <gauthamns@users.noreply.github.com>
2020-06-28 13:51:25 +0530

    Adding preferences and user data for tracking user details

---
+++ lib/config/prefs.dart
@@ -0,0 +1,53 @@
+import 'package:hack20/data/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Prefs {
+  static final Prefs _instance = Prefs._internal();
+
+  static const String KEY_USER_NAME = 'userName';
+  static const String KEY_USER_AGE = 'userAge';
+  static const String KEY_USER_GENDER = 'userGender';
+
+  SharedPreferences sharedPrefs;
+
+  User loggedUser;
+
+  Prefs._internal();
+
+  // Load the preferences in the background using async
+  // True if you loading happened. Else false if already loaded.
+  Future<bool> load() async {
+    if (!isSharedPrefsAvailable()) {
+      sharedPrefs = await SharedPreferences.getInstance();
+      loggedUser = loadUserDetails();
+    }
+  }
+
+  loadUserDetails() {
+    loggedUser = User();
+    if (isUserSignedIn()) {
+      loggedUser.name = getUserName();
+      loggedUser.age = sharedPrefs.getInt(KEY_USER_AGE);
+      loggedUser.gender = sharedPrefs.getString(KEY_USER_GENDER);
+    }
+  }
+
+  // If we have user name, then user is signed in.
+  bool isUserSignedIn() {
+    String name = getUserName();
+    return name != null && name != "";
+  }
+
+  bool isSharedPrefsAvailable() {
+    return sharedPrefs != null;
+  }
+
+  // UserID.
+  String getUserName() {
+    if (isSharedPrefsAvailable()) {
+      return sharedPrefs.getString(KEY_USER_NAME);
+    }
+
+    return null;
+  }
+}
